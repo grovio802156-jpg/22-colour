13b7cf646e77dd2abb5bce512a8cbf8b715f65d6
grovio802156-jpg <grovio802156@gmail.com>
2026-03-07 09:50:51 +0530

    Implement deposit and withdraw functionality
    Added deposit and withdraw functions to manage balance.

--- script.js
+++ script.js
@@ -1,9 +1,71 @@
-function showPage(page){
+let balance = 1200;
 
-document.querySelectorAll(".page").forEach(p=>{
-p.classList.add("hidden");
-});
+function deposit(){
 
-document.getElementById(page).classList.remove("hidden");
+let amount = prompt("Enter deposit amount");
+
+if(amount){
+
+balance += parseInt(amount);
+
+updateBalance();
+
+}
+
+}
+
+function withdraw(){
+
+let amount = prompt("Enter withdraw amount");
+
+if(amount <= balance){
+
+balance -= parseInt(amount);
+
+updateBalance();
+
+}
+
+else{
+
+alert("Not enough balance");
+
+}
+
+}
+
+function updateBalance(){
+
+document.getElementById("balance").innerText = "₹" + balance;
+
+}
+
+function openGame(name){
+
+alert(name + " game starting");
+
+}
+
+function home(){
+
+alert("Home Page");
+
+}
+
+function activity(){
+
+alert("Activity Page");
+
+}
+
+function promotion(){
+
+alert("Promotion Page");
+
+}
+
+function account(){
+
+alert("Account Page");
 
 }
