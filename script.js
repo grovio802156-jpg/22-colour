f08c21c633841a24c4ca644d79fc5529e502e719
grovio802156-jpg <grovio802156@gmail.com>
2026-03-15 11:12:54 +0530

    Improve formatting and structure in script.js
    Refactor code for consistency and readability.

--- script.js
+++ script.js
@@ -1,76 +1,82 @@
-let mode="html"
+let mode = "html";
 
-let htmlCode=""
-let cssCode=""
-let jsCode=""
+let htmlCode = "";
+let cssCode = "";
+let jsCode = "";
+
+const codeArea = document.getElementById("codeArea");
+const result = document.getElementById("result");
+const errorBox = document.getElementById("errorBox");
 
 function setMode(type){
 
-mode=type
+mode = type;
 
-if(type=="html") codeArea.value=htmlCode
-if(type=="css") codeArea.value=cssCode
-if(type=="js") codeArea.value=jsCode
+if(type === "html"){
+codeArea.value = htmlCode;
+}
+
+if(type === "css"){
+codeArea.value = cssCode;
+}
+
+if(type === "js"){
+codeArea.value = jsCode;
+}
 
 }
 
 function updatePreview(){
 
-let code=codeArea.value
+let code = codeArea.value;
 
-if(mode=="html") htmlCode=code
-if(mode=="css") cssCode=code
-if(mode=="js") jsCode=code
+if(mode === "html"){
+htmlCode = code;
+}
 
-let finalCode=`
+if(mode === "css"){
+cssCode = code;
+}
 
+if(mode === "js"){
+jsCode = code;
+}
+
+let finalCode = `
+<!DOCTYPE html>
 <html>
-
-<style>${cssCode}</style>
+<head>
+<style>
+${cssCode}
+</style>
+</head>
 
 <body>
 
 ${htmlCode}
 
-<script>${jsCode}<\/script>
+<script>
+${jsCode}
+<\/script>
 
 </body>
-
 </html>
+`;
 
-`
-
-result.srcdoc=finalCode
-
-checkError()
-
-}
-
-function checkError(){
-
-let text=codeArea.value
-
-let errors=[]
-
-if(text.includes("console.error")){
-
-errors.push("Console error detected")
-
-}
-
-errorBox.innerHTML=errors.join("<br>")
+result.srcdoc = finalCode;
 
 }
 
 function newProject(){
 
-if(confirm("Start new project?")){
+if(confirm("Create new project?")){
 
-htmlCode=""
-cssCode=""
-jsCode=""
-codeArea.value=""
-result.srcdoc=""
+htmlCode = "";
+cssCode = "";
+jsCode = "";
+
+codeArea.value = "";
+result.srcdoc = "";
 
 }
 
@@ -78,31 +84,76 @@ result.srcdoc=""
 
 function saveProject(){
 
-localStorage.setItem("html",htmlCode)
-localStorage.setItem("css",cssCode)
-localStorage.setItem("js",jsCode)
+localStorage.setItem("htmlCode", htmlCode);
+localStorage.setItem("cssCode", cssCode);
+localStorage.setItem("jsCode", jsCode);
 
-alert("Project Saved")
+alert("Project Saved");
 
 }
 
 function exportProject(){
 
-let file=`
-
+let file = `
 <!DOCTYPE html>
 <html>
-<style>${cssCode}</style>
+<head>
+<style>
+${cssCode}
+</style>
+</head>
+
 <body>
 
 ${htmlCode}
 
-<script>${jsCode}<\/script>
+<script>
+${jsCode}
+<\/script>
 
 </body>
 </html>
+`;
 
-`
+let blob = new Blob([file], {type:"text/html"});
+
+let a = document.createElement("a");
+
+a.href = URL.createObjectURL(blob);
+a.download = "myApp.html";
+a.click();
+
+}
+
+function dragDrop(){
+
+alert("Drag & Drop Builder Coming Soon");
+
+}
+
+function settings(){
+
+alert("Settings Panel Coming Soon");
+
+}
+
+window.onload = function(){
+
+let savedHTML = localStorage.getItem("htmlCode");
+let savedCSS = localStorage.getItem("cssCode");
+let savedJS = localStorage.getItem("jsCode");
+
+if(savedHTML){
+htmlCode = savedHTML;
+cssCode = savedCSS;
+jsCode = savedJS;
+
+codeArea.value = htmlCode;
+
+updatePreview();
+}
+
+}`
 
 let blob=new Blob([file],{type:"text/html"})
 
